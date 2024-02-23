1f892b473b107d9fb523bc853e2bcf6d3f3fe3fe
utsav2501 <utsav2112088@akgec.ac.in>
2024-02-23 21:34:02 +0530

    second commit

---
+++ backend/server.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"",
+    database: "signup"
+})
+
+app.post('/login', (req,res) => {
+    const sql = "SELECT * FROM login WHERE `email` = ? AND `password`= ? "
+    
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        if(data.length > 0) {
+            return res.json("Success")
+        }
+        else{
+            return res.json("failed")
+        }
+    })
+})
+app.post('/signup', (req,res) => {
+    const sql = "INSERT INTO login (`name`,`email`,`password` ) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+
+app.post('/booking', (req,res) => {
+    const sql = "INSERT INTO booking (`name`,`email`,`phone`, `dob`, `message` ) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.dob,
+        req.body.message
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.listen(8081, ()=> {
+    console.log("listening");
+})
